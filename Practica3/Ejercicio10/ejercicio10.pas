c5e892b034572e3e6b533ae24f0015469b3af94b
Nazareno <stufanonaza@hotmail.com>
2024-04-13 17:44:58 -0300

    Ejercicio 10 finalizado

--- Practica3/Ejercicio10/ejercicio10.pas
+++ Practica3/Ejercicio10/ejercicio10.pas
@@ -1,5 +1,41 @@
 program Ejercicio10;
 
+(*Ej 10) Leer desde un archivo números enteros ordenados de menor a mayor, puede haber elementos
+repetidos.
+Calcular y mostrar cada número con su respectiva frecuencia.
+Ejemplo: 2, 2, 3, 3, 3, 3, 7, 9, 9, 9 
+
+Numero Frecuencia
+    2       2
+    3       4
+    7       1
+    9       3
+*)
+var
+    arch: text;
+    ant, numero, repeticiones: integer;
 begin
-    
+    assign(arch, 'datos.TXT');
+    reset(arch);
+    readln(arch, ant);
+    repeticiones := 1;
+
+    writeln('Numero - Frecuencia');
+
+    while NOT eof (arch) do
+        begin
+            readln(arch, numero);
+
+            if (numero = ant) then
+                repeticiones := repeticiones + 1;
+            
+            if (numero <> ant) or (eof(arch)) then
+                begin
+                    writeln(ant, ' - ', repeticiones);
+                    repeticiones := 1;
+                end;
+            ant := numero;   
+        end;
+    close(arch);
+    readln();
 end.
