5ba29e18d64a0d79473b3d127a2d824068557f01
Nazareno <stufanonaza@hotmail.com>
2024-04-16 01:29:56 -0300

    Ejercicio 18 finalizado

---
+++ Practica3/Ejercicio18/ejercicio18.pas
@@ -0,0 +1,72 @@
+program Ejercicio18;
+
+(*Ej 18) Reescribir el ejercicio 17, considerando que los datos están grabados en un archivo (con el formato
+que considere más adecuado). *)
+
+const 
+    desc = 0.1;
+
+var
+    arch:text;
+    precio, mayCantArtOferta, contCliSoloOfer, contArticulosOferta, montoTotal:integer;
+    descuento, aux: char;
+    cliente, nombreMayCantArtOfer: string;
+    totalDescuento, totalAPagar: real;
+    soloOferta: boolean;
+
+begin
+    assign(arch, 'datos.TXT');
+    reset(arch);
+
+    contArticulosOferta := 0;
+    mayCantArtOferta := 0;
+    soloOferta := true;
+
+    read(arch, cliente);
+    while (not eof(arch) ) do
+        begin
+            read(arch, precio, aux, descuento);
+            
+            repeat
+                if descuento = 'S' then
+                    begin
+                        totalDescuento := totalDescuento + (precio*desc);
+                        montoTotal := montoTotal + precio;
+                        contArticulosOferta := contArticulosOferta +1;
+                    end
+                else
+                    begin
+                        soloOferta := false;
+                        montoTotal := montoTotal + precio;                            
+                    end;
+                totalAPagar := montoTotal - totalDescuento;
+                read(arch, precio, aux, descuento); 
+            until (precio = 0);
+
+            if mayCantArtOferta<contArticulosOferta then
+                begin
+                    mayCantArtOferta := contArticulosOferta;
+                    nombreMayCantArtOfer := cliente;
+                end;
+        
+            readln(arch, cliente);
+
+            if cliente = '*****' then
+                begin
+                    if soloOferta then
+                        contCliSoloOfer := contCliSoloOfer + 1;
+                    
+                    writeln('El monto total de la vente es: ', montoTotal, '. El descuento aplicado es: ', totalDescuento:0:2, '. El total a pagar es: ', totalAPagar:0:2);
+                    
+                    soloOferta := true;
+                    totalAPagar := 0;
+                    totalDescuento := 0;
+                    montoTotal:= 0;
+                    contArticulosOferta := 0;
+
+                    read(arch, cliente);
+                end;
+        end;    
+    writeln('El cliente que mas articulos en oferta compro es: ', nombreMayCantArtOfer);
+    writeln('La cantidad de clientes que compraron unicamente en oferta son: ', contCliSoloOfer);
+end.
